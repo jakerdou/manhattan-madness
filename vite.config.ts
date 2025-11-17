9e9fc8a6898f0ed46ffe47662427ec139800b3e5
jakerdou <hem.adtigo@gmail.com>
2025-11-17 11:53:22 -0500

    add secrets

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/manhattan-madness2/',
+  base: '/manhattan-madness/',
 })
